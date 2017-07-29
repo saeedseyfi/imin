a50c99a8171c5e747c80cc9bb0b7aaa0ff772c8f
saeedseyfi <me@saeedseyfi.com>
2017-07-29 17:08:33 +0430

    Update Event model

--- models/Event.js
+++ models/Event.js
@@ -3,9 +3,11 @@ const crypto = require('crypto');
 class Event {
     constructor(options) {
         this.id = options.id || crypto.createHash('sha1').update((new Date()).valueOf().toString() + Math.random().toString()).digest('hex');
+        this.owner = options.owner || null;
         this.messageId = options.messageId || null;
-        this.name = options.name || null;
-        this.attendees = options.attendees || [];
+        this.title = options.title || null;
+        this.willAttend = options.willAttend || [];
+        this.wontAttend = options.wontAttend || [];
         this.chatId = options.chatId || null;
     }
 }
